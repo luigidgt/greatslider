2dfa316933576dfcc3d09785e852294fa0b9504b
dpisconte <dpisconte@dgtalliance.com>
2018-05-24 19:12:33 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,42 +1,49 @@
-
-let $miSlider = $('#example1').greatSlider({
-	type: 'swipe',
-	nav: true,
-	navSpeed: 500,
-	lazyLoad: true
-});
-
-
-$('#go').click(()=>{
-	$miSlider.goTo(5);
-});
-
-$('#play').click(()=>{
-	$miSlider.autoPlay('play');
-});
-
-$('#stop').click(()=>{
-	$miSlider.autoPlay('stop');
-});
-
-$('#next').click(()=>{
-	$miSlider.goTo('next');
-});
-
-$('#prev').click(()=>{
-	$miSlider.goTo('prev');
-});
-
-$('#fs').click(()=>{
-	//fullScreenApi.requestFullScreen($('.cualquiera').get(0));
-	$miSlider.fullscreen('in');
-});
-
-
 /*
-let $example2 = $('#example2').greatSlider({
+
+	let $miSlider = $('#example1').greatSlider({
+		type: 'swipe',
+		nav: true,
+		navSpeed: 500,
+		lazyLoad: true,
+		bullets: true,
+		items: 3,
+		breakPoints: {
+			1280: {
+				items: 1
+			}
+		}
+	});
+
+	$('#go').click(()=>{
+		$miSlider.goTo(5);
+	});
+
+	$('#play').click(()=>{
+		$miSlider.autoPlay('play');
+	});
+
+	$('#stop').click(()=>{
+		$miSlider.autoPlay('stop');
+	});
+
+	$('#next').click(()=>{
+		$miSlider.goTo('next');
+	});
+
+	$('#prev').click(()=>{
+		$miSlider.goTo('prev');
+	});
+
+	$('#fs').click(()=>{
+		//fullScreenApi.requestFullScreen($('.cualquiera').get(0));
+		$miSlider.fullscreen('in');
+	});
+*/
+
+// Slider de testimonios
+$('#testimonios').greatSlider({
 	type: 'swipe',
+	//autoHeight: true;
 	nav: false,
 	bullets: true
 });
-*/
